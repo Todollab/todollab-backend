efa310693d2383ce64c947310cf0a451c72e56db
Bruno <bcgmeireles@gmail.com>
2025-07-02 11:35:39 -0300

    initializes repository

---
+++ src/main/java/com/bruno/collab_todo/CollabTodoApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.collab_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/collab_todo/CollabTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.collab_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
